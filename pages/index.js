6cbe9b8381c2c821628b2a491b66802d56b77602
halisycel <isaycel2001@gmail.com>
2022-04-09 11:45:20 +0300

    🎉 Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Input from '../components/input';
+
+const Home = () => {
+    return (
+        <div>
+            <Input />
+        </div>
+    );
+};
+
+export default Home;
---
+++ stories/components/input.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Input from '../../components/input';
+import '../../styles/global.css';
+
+export default {
+    title: 'components/Input',
+    component: Input,
+};
+
+export const Default = () => <Input />;
