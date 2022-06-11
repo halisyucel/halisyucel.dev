63960cddb18f452641a75a39c2cae863649d9863
halisycel <isaycel2001@gmail.com>
2022-06-11 18:05:07 +0300

    add header

---
+++ components/header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import NavLink from './navlink';
+
+const Header: React.FC = () => {
+	return (
+		<header className={'fixed top-0 left-0 w-full h-20 border-b border-gray'}>
+			<div
+				className={
+					'absolute z-0 overflow-hidden w-full h-full bg-black/25 backdrop-blur top-0 left-0'
+				}
+			/>
+			<nav
+				className={
+					'flex justify-center items-center absolute top-0 left-0 z-10 w-full h-full'
+				}
+			>
+				<NavLink href={'/'} label={'Home'} />
+				<NavLink href={'/projects'} label={'Projects'} />
+				<NavLink href={'/blog'} label={'Blog'} />
+				<NavLink href={'/bookmarks'} label={'Bookmarks'} />
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
---
+++ components/navlink.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+interface NavLink {
+	href: string;
+	label: string;
+}
+
+const NavLink: React.FC<NavLink> = ({ href, label }) => {
+	const router = useRouter();
+	return (
+		<Link href={href}>
+			<a
+				className={`text-4xl h-12 font-extrabold last:mr-0 mr-6${
+					router.pathname === href ? ' text-gradient' : ''
+				}`}
+			>
+				{label}
+			</a>
+		</Link>
+	);
+};
+
+export default NavLink;
