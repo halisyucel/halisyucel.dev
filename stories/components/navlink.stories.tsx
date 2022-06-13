c0f91f4efdea1c35adbd7c0dab4119b14a222335
halisycel <isaycel2001@gmail.com>
2022-06-13 15:11:29 +0300

    storybook integration

---
+++ stories/components/navlink.stories.tsx
@@ -0,0 +1,8 @@
+import NavLink from '../../components/navlink';
+
+export default {
+	title: "Components/NavLink",
+	component: NavLink,
+};
+
+export const Navlink = () => <NavLink  href={'/'} label={'Home'}/>
