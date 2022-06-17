9d5e6617a560fe38af4abaf42e3b826933778b7c
halisycel <isaycel2001@gmail.com>
2022-06-17 20:27:22 +0300

    some configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import './global.css';
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function HalisYucelMe({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default HalisYucelMe;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
 
 class MyDocument extends Document {
 	static async getInitialProps(ctx: DocumentContext) {
@@ -11,10 +11,16 @@ class MyDocument extends Document {
 			<Html lang={'tr'}>
 				<Head>
 					<link rel={'preconnect'} href={'https://fonts.googleapis.com'} />
-					<link rel={'preconnect'} href={'https://fonts.gstatic.com'} crossOrigin={'anonymous'} />
 					<link
-						href={'https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Signika:wght@300;400;500;600;700&display=swap'}
+						rel={'preconnect'}
+						href={'https://fonts.gstatic.com'}
+						crossOrigin={'anonymous'}
+					/>
+					<link
 						rel={'stylesheet'}
+						href={
+							'https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Signika:wght@300;400;500;600;700&display=swap'
+						}
 					/>
 				</Head>
 				<body className={'bg-black text-white mt-20 pt-20 flex justify-center items-start'}>
