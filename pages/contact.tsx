195eecb3c2f349372d1fa4bcaa318571078c64f0
halisycel <isaycel2001@gmail.com>
2022-06-18 02:54:24 +0300

    add content contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,34 +1,38 @@
+import Button from '../components/button';
+import Divider from '../components/divider';
 import Input from '../components/input';
 import Layout from '../components/layout';
 import { NextPage } from 'next';
 import React from 'react';
-import Divider from '../components/divider';
 
 const Contact: NextPage = () => {
 	return (
 		<Layout>
 			<h1 className={'text-5xl font-title font-extrabold text-gradient w-fit'}>Contact me</h1>
-			<p>
-				naber lan
+			<p className={'mt-6 font-text font-light text-xl'}>
+				You can send an e-mail to &nbsp;
+				<a
+					className={'text-gradient font-extrabold'}
+					href={`mailto:${process.env.NEXT_PUBLIC_EMAIL}`}
+				>
+					{process.env.NEXT_PUBLIC_EMAIL}
+				</a>
+				&nbsp;or fill out the form below to contact me.
 			</p>
 			<Divider />
-			<Input
-				type={'text'}
-				className={'mb-6'}
-				label={'Name'}
-				placeholder={'Your name'}
-			/>
-			<Input
-				type={'text'}
-				className={'mb-6'}
-				label={'Email'}
-				placeholder={'Your email'}
-			/>
-			<Input
-				type={'textarea'}
-				label={'Message'}
-				placeholder={'Your message'}
-			/>
+			<Input type={'text'} className={'mb-6'} label={'Name'} placeholder={'Your name'} />
+			<form>
+				<Input
+					type={'text'}
+					className={'mb-6'}
+					label={'Email'}
+					placeholder={'Your email'}
+				/>
+				<Input type={'textarea'} label={'Message'} placeholder={'Your message'} />
+				<Button type={'submit'} loading={false}>
+					Send
+				</Button>
+			</form>
 		</Layout>
 	);
 };
