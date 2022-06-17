8cac7dd92a4cc5210c346e4be4fc8e34c0d947a0
halisycel <isaycel2001@gmail.com>
2022-06-18 02:53:52 +0300

    add footer component

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+	return (
+		<footer className={'w-full h-80 bg-gray mt-28'}>
+			ehu
+		</footer>
+	);
+};
+
+export default Footer;
