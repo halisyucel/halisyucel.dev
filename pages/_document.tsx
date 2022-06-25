3b8442510abdc4934c9e2bb25039e40e00bba280
halisycel <isaycel2001@gmail.com>
2022-06-25 23:07:47 +0300

    some configuration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,15 @@
-import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
 
-class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return { ...initialProps };
-	}
-	render() {
-		return (
-			<Html lang={'tr'}>
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
-}
 
-export default MyDocument;
+export default function Document() {
+	return (
+		<Html>
+			<Head />
+			<body className={'h-[100vh] min-h-[100vh] p-10'}>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+	darkMode: ['class', '[data-mode="dark"]'],
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
@@ -7,33 +9,10 @@ module.exports = {
 				main: '40rem',
 			},
 			colors: {
-				black: '#010409',
-				white: '#ffffff',
-				gray: '#333333',
-				purple: '#c471ed',
-				lightblue: '#12c2e9',
-				red: '#f64f59',
-				green: '#4caf50',
-			},
-			fontFamily: {
-				text: ['Roboto', 'sans-serif'],
-				title: ['Raleway', 'sans-serif'],
-			},
-			borderRadius: {
-				inherit: 'inherit',
-			},
-			borderColor: {
-				loading: '#fff transparent transparent transparent',
-			},
-			boxShadow: {
-				white: '0 0 0 4px rgba(255, 255, 255, 1)',
-			},
-			spacing: {
-				input: 'calc(100% - 2rem)',
-			},
-			animation: {
-				loading: 'spin 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite',
-			},
+				'bg-primary': '#ffffff',
+				'bg-secondary': '#f0f0f2',
+				'border': '#d9d9d9'
+			}
 		},
 	},
 	plugins: [],
