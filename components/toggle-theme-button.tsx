d027b4810629eb0bba29e13da8929bd2b6f8a6c9
halisycel <isaycel2001@gmail.com>
2022-06-26 04:47:33 +0300

    change iconbutton instead of button

--- components/toggle-theme-button.tsx
+++ components/toggle-theme-button.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import { Button } from 'rsuite';
-import { BsFillSunFill, BsFillMoonStarsFill } from 'react-icons/bs';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../redux/store';
 import { toggleTheme } from '../redux/features/settings';
+import { RootState } from '../redux/store';
+import React from 'react';
+import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
+import { useDispatch, useSelector } from 'react-redux';
+import { IconButton } from 'rsuite';
+
+// import { elementToSVG } from 'dom-to-svg';
 
 const ToggleThemeButton = () => {
 	const dispatch = useDispatch();
 	const { theme } = useSelector((state: RootState) => state.settings);
+	// const [svgData, setSvgData] = useState('');
 	return (
-		<Button
-			size={'xs'}
-			onClick={() => dispatch(toggleTheme())}
-		>
-			{theme === 'light' ?
-				<BsFillSunFill className={'h-5 text-yellow-500'}/> :
-				<BsFillMoonStarsFill className={'h-5 text-yellow-500'}/>
-			}
-		</Button>
+		<React.Fragment>
+			<IconButton
+				size={'xs'}
+				onClick={() => {
+					dispatch(toggleTheme());
+					// const svgDocument = elementToSVG(document.body);
+					// const svgString = new XMLSerializer().serializeToString(svgDocument);
+					// setSvgData(svgString);
+				}}
+				icon={
+					theme === 'light' ? (
+						<BsFillSunFill className={'text-yellow-500'} />
+					) : (
+						<BsFillMoonStarsFill className={'text-yellow-500'} />
+					)
+				}
+			/>
+			{/*<div
+				className={`fixed inset-0 w-screen ${svgData === '' ? 'hidden' : 'visible'}`}
+				dangerouslySetInnerHTML={{ __html: svgData }}
+			/>*/}
+		</React.Fragment>
 	);
 };
 
